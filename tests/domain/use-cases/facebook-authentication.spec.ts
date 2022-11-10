cbc6e80b92b91fd54fe85da0b86e6bec6a2c37bf
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-10 16:52:14 -0300

    test: ensure use case returns AuthenticationError when LoadFacebookUserApi returns undefined

--- tests/domain/use-cases/facebook-authentication.spec.ts
+++ tests/domain/use-cases/facebook-authentication.spec.ts
@@ -1,11 +1,14 @@
 import { LoadFacebookUserApi } from '@/domain/contracts/apis'
+import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthenticationUseCase } from '@/domain/use-cases'
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
+  result = undefined
 
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token
+    return this.result
   }
 }
 
@@ -17,5 +20,15 @@ describe('FacebookAuthenticationUseCase', () => {
     await sut.perform({ token: 'any_token' })
 
     expect(loadFacebookUserApi.token).toBe('any_token')
+  })
+
+  it('Should return AuthenticationError when LoadFacebookUserApi returns undefined', async (): Promise<void> => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    loadFacebookUserApi.result = undefined
+    const sut = new FacebookAuthenticationUseCase(loadFacebookUserApi)
+
+    const authResult = await sut.perform({ token: 'any_token' })
+
+    expect(authResult).toEqual(new AuthenticationError())
   })
 })
