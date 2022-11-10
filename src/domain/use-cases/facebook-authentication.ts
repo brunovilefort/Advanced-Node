23882eaaccd728a54645dd861cfea564d49fef92
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-10 16:51:48 -0300

    feat: ensure use case returns AuthenticationError when LoadFacebookUserApi returns undefined

--- src/domain/use-cases/facebook-authentication.ts
+++ src/domain/use-cases/facebook-authentication.ts
@@ -1,10 +1,12 @@
 import { LoadFacebookUserApi } from '@/domain/contracts/apis'
 import { FacebookAuthentication } from '@/domain/features'
+import { AuthenticationError } from '@/domain/errors'
 
 export class FacebookAuthenticationUseCase {
   constructor (private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
 
-  async perform (params: FacebookAuthentication.Params): Promise<void> {
+  async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     await this.loadFacebookUserApi.loadUser(params)
+    return new AuthenticationError()
   }
 }
