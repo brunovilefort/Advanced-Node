7d5c9408fc0a860123024490b6747a1094378c9a
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-10 20:20:09 -0300

    feat: ensure use case calls CreateFacebookAccountRepo when LoadUserAccountRepository returns undefined

--- src/domain/use-cases/facebook-authentication.ts
+++ src/domain/use-cases/facebook-authentication.ts
@@ -1,18 +1,20 @@
 import { LoadFacebookUserApi } from '@/domain/contracts/apis'
 import { FacebookAuthentication } from '@/domain/features'
 import { AuthenticationError } from '@/domain/errors'
-import { LoadUserAccountRepository } from '@/domain/contracts/repos'
+import { LoadUserAccountRepository, CreateFacebookAccountRepository } from '@/domain/contracts/repos'
 
 export class FacebookAuthenticationUseCase {
   constructor (
     private readonly loadFacebookUserApi: LoadFacebookUserApi,
-    private readonly loadUserAccountRepo: LoadUserAccountRepository
+    private readonly loadUserAccountRepo: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepo: CreateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.loadFacebookUserApi.loadUser(params)
     if (fbData !== undefined) {
       await this.loadUserAccountRepo.load({ email: fbData.email })
+      await this.createFacebookAccountRepo.createFromFacebook(fbData)
     }
     return new AuthenticationError()
   }
