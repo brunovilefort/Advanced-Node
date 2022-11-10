4d2bf3adec84b69766f2bc9137bbb523c4ca7b32
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-10 20:04:04 -0300

    feat: ensure use case calls LoadUserAccountRepo when LoadFacebookUserApi returns data

--- src/domain/use-cases/facebook-authentication.ts
+++ src/domain/use-cases/facebook-authentication.ts
@@ -1,12 +1,19 @@
 import { LoadFacebookUserApi } from '@/domain/contracts/apis'
 import { FacebookAuthentication } from '@/domain/features'
 import { AuthenticationError } from '@/domain/errors'
+import { LoadUserAccountRepository } from '@/domain/contracts/repos'
 
 export class FacebookAuthenticationUseCase {
-  constructor (private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
+  constructor (
+    private readonly loadFacebookUserApi: LoadFacebookUserApi,
+    private readonly loadUserAccountRepo: LoadUserAccountRepository
+  ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    await this.loadFacebookUserApi.loadUser(params)
+    const fbData = await this.loadFacebookUserApi.loadUser(params)
+    if (fbData !== undefined) {
+      await this.loadUserAccountRepo.load({ email: fbData.email })
+    }
     return new AuthenticationError()
   }
 }
