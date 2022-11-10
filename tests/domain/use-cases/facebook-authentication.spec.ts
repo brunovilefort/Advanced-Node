d8afeda79610de5a9d243141dd278cf417270f33
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-10 19:30:45 -0300

    refactor: simplify code

--- tests/domain/use-cases/facebook-authentication.spec.ts
+++ tests/domain/use-cases/facebook-authentication.spec.ts
@@ -7,6 +7,7 @@ import { mock, MockProxy } from 'jest-mock-extended'
 describe('FacebookAuthenticationUseCase', () => {
   let sut: FacebookAuthenticationUseCase
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
+  const token = 'any_token'
 
   beforeEach(() => {
     loadFacebookUserApi = mock()
@@ -14,16 +15,16 @@ describe('FacebookAuthenticationUseCase', () => {
   })
 
   it('Should call LoadFacebookUserApi with correct params', async (): Promise<void> => {
-    await sut.perform({ token: 'any_token' })
+    await sut.perform({ token })
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token })
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('Should return AuthenticationError when LoadFacebookUserApi returns undefined', async (): Promise<void> => {
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
 
-    const authResult = await sut.perform({ token: 'any_token' })
+    const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
