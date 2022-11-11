6562e849bd2303a846b34f8b2efc8c26add97c13
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-11 17:41:13 -0300

    feat: ensure use case calls TokenGenerator with correct params

--- src/domain/use-cases/facebook-authentication.ts
+++ src/domain/use-cases/facebook-authentication.ts
@@ -1,4 +1,4 @@
-import { LoadFacebookUserApi } from '@/domain/contracts/apis'
+import { LoadFacebookUserApi, TokenGenerator } from '@/domain/contracts/apis'
 import { FacebookAuthentication } from '@/domain/features'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAccount } from '@/domain/models'
@@ -7,7 +7,8 @@ import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '@/doma
 export class FacebookAuthenticationUseCase {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository
+    private readonly userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository,
+    private readonly crypto: TokenGenerator
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
@@ -15,7 +16,8 @@ export class FacebookAuthenticationUseCase {
     if (fbData !== undefined) {
       const accounData = await this.userAccountRepo.load({ email: fbData.email })
       const fbAccount = new FacebookAccount(fbData, accounData)
-      await this.userAccountRepo.saveWithFacebook(fbAccount)
+      const { id } = await this.userAccountRepo.saveWithFacebook(fbAccount)
+      await this.crypto.generateToken({ key: id })
     }
     return new AuthenticationError()
   }
