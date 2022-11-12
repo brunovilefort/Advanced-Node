b79210bf08991cb9f4e33d58ab632bd4529d5f10
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-12 15:11:28 -0300

    feat: ensure FacebookApi get app token with correct params

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -4,9 +4,20 @@ import { LoadFacebookUserApi } from '@/domain/contracts/apis'
 export class FacebookApi {
   private readonly baseUrl = 'https://graph.facebook.com'
 
-  constructor (private readonly httpClient: HttpGetClient) {}
+  constructor (
+    private readonly httpClient: HttpGetClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string
+  ) {}
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
-    await this.httpClient.get({ url: `${this.baseUrl}/oauth/access_token` })
+    await this.httpClient.get({
+      url: `${this.baseUrl}/oauth/access_token`,
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: 'client_credentials'
+      }
+    })
   }
 }
