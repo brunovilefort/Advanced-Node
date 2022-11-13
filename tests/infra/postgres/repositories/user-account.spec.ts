f48bb8f5923628c04300e2b37e58af78e3a6a273
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-12 23:22:08 -0300

    test: ensure PgUserAccountRepository returns account id

--- tests/infra/postgres/repositories/user-account.spec.ts
+++ tests/infra/postgres/repositories/user-account.spec.ts
@@ -43,7 +43,7 @@ describe('PgUserAccountRepository', () => {
 
   describe('SaveWithFacebook', () => {
     it('Should create an account if id is undefined', async () => {
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         email: 'any_email',
         name: 'any_name',
         facebookId: 'eany_fb_id'
@@ -51,6 +51,7 @@ describe('PgUserAccountRepository', () => {
       const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
 
       expect(pgUser?.id).toBe(1)
+      expect(id).toBe('1')
     })
 
     it('Should update account if id is defined', async () => {
@@ -60,7 +61,7 @@ describe('PgUserAccountRepository', () => {
         facebookId: 'any_fb_id'
       })
 
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         id: '1',
         email: 'new_email',
         name: 'new_name',
@@ -74,6 +75,7 @@ describe('PgUserAccountRepository', () => {
         name: 'new_name',
         facebookId: 'new_fb_id'
       })
+      expect(id).toBe('1')
     })
   })
 })
