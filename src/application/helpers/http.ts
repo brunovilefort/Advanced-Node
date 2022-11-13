127eeef9eb2a1edfc5b3819a9b276b74cf1ae011
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-13 11:37:15 -0300

    refactor: move production code to their own files

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,11 +1,3 @@
-export class ServerError extends Error {
-  constructor (error?: Error) {
-    super('Server failed. Try again soon')
-    this.name = 'ServerError'
-    this.stack = error?.stack ?? undefined
-  }
-}
-
 export type HttpResponse = {
   statusCode: number
   data: any
