1d85735395a416c7a47c1177fadf42b4d3b6b40e
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-15 20:38:28 -0300

    feat: transform ExpressRouter to a function

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,7 +1,8 @@
+import { adaptExpressRoute as adapt } from '@/main/adapters'
+import { makeFacebookLoginController as facebookLogin } from '@/main/factories'
+
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/api/login/facebook', (req, res) => {
-    res.send({ data: 'any_data' })
-  })
+  router.post('/api/login/facebook', adapt(facebookLogin()))
 }
