c97c7ceeb6cdd4eb64c89358e1c3219075ec22ae
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-15 13:01:35 -0300

    feat: ensure FacebookApi returns undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '3262311390765568',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '9a67fac4ad27e0b8f69a0148391dfad3'
+  }
+}
