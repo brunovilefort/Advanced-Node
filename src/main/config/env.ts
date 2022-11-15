9c21fba8f99a26b54e862cd6dd8f5d4fae8b9f3f
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-15 13:38:41 -0300

    chore: add express packages

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,6 +2,7 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '3262311390765568',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '9a67fac4ad27e0b8f69a0148391dfad3',
-    facebookToken: process.env.FB_TOKEN ?? 'EAAuXDgZBX4gABAOR4sJAkoRtyfEyTOu7yCo3kWJ5PGZBy3vobUb35F5COB9tZCufgXarZCtTyJVdG7CXdpKB9dksZCZBbKdxnZAdf0JFNlOSOjfm2i2EWQFLwFwqddxKdPBK6VTkSwHjFioK0DNalYNZAYD7Ik6PNGx8prvp2PfJrzF5nNZCBZCLZAFYJK0jXTO52VwgtR4A7k4N2uR9gJx01Rl'
+    facebookToken: process.env.FB_TOKEN ?? 'EAAuXDgZBX4gABAOR4sJAkoRtyfEyTOu7yCo3kWJ5PGZBy3vobUb35F5COB9tZCufgXarZCtTyJVdG7CXdpKB9dksZCZBbKdxnZAdf0JFNlOSOjfm2i2EWQFLwFwqddxKdPBK6VTkSwHjFioK0DNalYNZAYD7Ik6PNGx8prvp2PfJrzF5nNZCBZCLZAFYJK0jXTO52VwgtR4A7k4N2uR9gJx01Rl',
+    port: process.env.PORT ?? '3333'
   }
 }
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,2 +1,13 @@
 import './config/module-alias'
+import { env } from './config/env'
+
 import 'reflect-metadata'
+import express from 'express'
+
+const port = env.facebookApi.port
+
+const app = express()
+
+app.listen(port, () =>
+  console.log(`🏃 Server running at http://localhost:${port}.`)
+)
