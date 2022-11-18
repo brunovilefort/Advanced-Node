0ba51a77a19b69be3abcd7eb14db2e5a89c14229
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-18 19:43:53 -0300

    refactor: improve code quality

--- src/application/middlewares/authentication.ts
+++ src/application/middlewares/authentication.ts
@@ -9,13 +9,17 @@ export class AuthenticationMiddleware {
   constructor (private readonly authorize: Authorize) {}
 
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    if (error !== undefined) return forbidden()
+    if (!this.validate({ authorization })) return forbidden()
     try {
       const userId = await this.authorize({ token: authorization })
       return success({ userId })
     } catch {
       return forbidden()
     }
+  }
+
+  private validate ({ authorization }: HttpRequest): boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    return error === undefined
   }
 }
