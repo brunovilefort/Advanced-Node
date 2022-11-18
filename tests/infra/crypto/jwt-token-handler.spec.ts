da9b922566bc0643e298e6984c5ba8c3ee8d2f6c
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-18 17:43:54 -0300

    test: ensure JwtTokenHandler call verify with correct params

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -18,7 +18,7 @@ describe('JwtTokenGenerator', () => {
     sut = new JwtTokenHandler(secret)
   })
 
-  describe('GenerateToken', () => {
+  describe('generateToken', () => {
     let key: string
     let expirationInMs: number
     let token: string
@@ -34,6 +34,7 @@ describe('JwtTokenGenerator', () => {
       await sut.generateToken({ key, expirationInMs })
 
       expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, { expiresIn: 1 })
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1)
     })
 
     it('Should return a token', async () => {
@@ -51,9 +52,18 @@ describe('JwtTokenGenerator', () => {
     })
   })
 
-  describe('', () => {
-    it('', () => {
+  describe('validateToken', () => {
+    let token: string
 
+    beforeAll(() => {
+      token = 'any_token'
+    })
+
+    it('Should call sign with correct params', async () => {
+      await sut.validateToken({ token })
+
+      expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret)
+      expect(fakeJwt.verify).toHaveBeenCalledTimes(1)
     })
   })
 })
