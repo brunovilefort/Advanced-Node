553cdec637dec1882ff378e944a1610b4025a787
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-20 11:13:20 -0300

    feat: ensure UUIDHandler returns correct value

--- src/infra/crypto/uuid-handler.ts
+++ src/infra/crypto/uuid-handler.ts
@@ -3,7 +3,7 @@ import { UUIDGenerator } from '@/domain/contracts/gateways'
 import { v4 } from 'uuid'
 
 export class UUIDHandler {
-  uuid ({ key }: UUIDGenerator.Input): void {
-    v4()
+  uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
+    return `${key}_${v4()}`
   }
 }
