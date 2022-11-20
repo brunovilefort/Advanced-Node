5e2af862884df8dbaba9d706fe2585597a467808
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-20 12:29:39 -0300

    fix: fix UniqueId to get new Date every call

--- tests/infra/gateways/unique-id.spec.ts
+++ tests/infra/gateways/unique-id.spec.ts
@@ -1,9 +1,20 @@
 import { UniqueId } from '@/infra/gateways'
 
-describe('UniqueId', () => {
-  it('Should call UUID.v4', () => {
-    const sut = new UniqueId(new Date(2022, 11, 20, 11, 22, 0))
+import { set, reset } from 'mockdate'
 
+describe('UniqueId', () => {
+  let sut: UniqueId
+
+  beforeAll(() => {
+    set(new Date(2022, 11, 20, 11, 22, 0))
+    sut = new UniqueId()
+  })
+
+  afterAll(() => {
+    reset()
+  })
+
+  it('Should create a unique id', () => {
     const uuid = sut.uuid({ key: 'any_key' })
 
     expect(uuid).toBe('any_key_202212202200000')
