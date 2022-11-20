ebeb51694e4ff9e8e62ed33e963624770ac24f7d
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-20 12:19:22 -0300

    test: add test to controller.spec.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,12 @@
 import './config/module-alias'
 import { env } from './config/env'
-import { app } from './config/app'
 
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 
 createConnection()
-  .then(() => app.listen(env.port, () => console.log(`🏃 Server running at http://localhost:${env.port}.`)))
+  .then(async () => {
+    const { app } = await import('./config/app')
+    app.listen(env.port, () => console.log(`🏃 Server running at http://localhost:${env.port}.`))
+  })
   .catch(console.error)
