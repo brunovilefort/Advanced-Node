db3525630843f02200c10b712524016239200d3b
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-20 13:34:57 -0300

    test: ensure AwsS3FileStorage config aws credentials upon creation

---
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -0,0 +1,31 @@
+import { AwsS3FileStorage } from '@/infra/gateways'
+
+import { config } from 'aws-sdk'
+
+jest.mock('aws-sdk')
+
+describe('AwsS3FileStorage', () => {
+  let sut: AwsS3FileStorage
+  let accessKey: string
+  let secret: string
+
+  beforeAll(() => {
+    accessKey = 'any_access_key'
+    secret = 'any_secret'
+  })
+
+  beforeEach(() => {
+    sut = new AwsS3FileStorage(accessKey, secret)
+  })
+
+  it('Should config aws credentials on creation', () => {
+    expect(sut).toBeDefined()
+    expect(config.update).toHaveBeenCalledWith({
+      credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secret
+      }
+    })
+    expect(config.update).toHaveBeenCalledTimes(1)
+  })
+})
