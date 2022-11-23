f118ba01d9a3a095da95caf897edea2529df3aeb
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-22 23:21:44 -0300

    test: ensure DeletePictureController returns 204

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -11,11 +11,20 @@ describe('DeletePictureController', () => {
   beforeEach(() => {
     sut = new DeletePictureController(changeProfilePicture)
   })
-  
+
   it('Should call ChangeProfilePicture with correct input', async () => {
     await sut.handle({ userId: 'any_user_id' })
 
     expect(changeProfilePicture).toHaveBeenCalledWith({ id: 'any_user_id' })
     expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return 204', async () => {
+    const httpResponse = await sut.handle({ userId: 'any_user_id' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 204,
+      data: null
+    })
   })
 })
