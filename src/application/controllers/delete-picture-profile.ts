bbab8894a0b3602da626553e66dcc65eb4e331a6
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-22 23:21:00 -0300

    feat: ensure DeletePictureController returns 204

--- src/application/controllers/delete-picture-profile.ts
+++ src/application/controllers/delete-picture-profile.ts
@@ -1,11 +1,13 @@
 import { ChangeProfilePicture } from '@/domain/use-cases'
+import { HttpResponse, noContent } from '../helpers'
 
 type HttpRequest = { userId: string }
 
 export class DeletePictureController {
   constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
 
-  async handle ({ userId }: HttpRequest): Promise<void> {
+  async handle ({ userId }: HttpRequest): Promise<HttpResponse> {
     await this.changeProfilePicture({ id: userId })
+    return noContent()
   }
 }
