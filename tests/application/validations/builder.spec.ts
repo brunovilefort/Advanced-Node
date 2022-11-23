400d6135aca3482207fa9742af5c1dfd660b3068
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-23 15:03:47 -0300

    test: add Required validator

--- tests/application/validations/builder.spec.ts
+++ tests/application/validations/builder.spec.ts
@@ -1,12 +1,12 @@
-import { RequiredStringValidator, ValidationBuilder } from '@/application/validations'
+import { RequiredString, ValidationBuilder } from '@/application/validations'
 
 describe('ValidationBuilder', () => {
-  it('Shoult return a RequiredStringValidator', () => {
+  it('Shoult return a RequiredString', () => {
     const validators = ValidationBuilder
       .of({ value: 'any_value', fieldName: 'any_name' })
       .required()
       .build()
 
-    expect(validators).toEqual([new RequiredStringValidator('any_value', 'any_name')])
+    expect(validators).toEqual([new RequiredString('any_value', 'any_name')])
   })
 })
