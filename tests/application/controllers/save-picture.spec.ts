933b0bcc57cd86fabb95b716405e02bb21a9a3d4
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-23 14:38:15 -0300

    test: ensure SavePictureController returns 200 with correct data

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -14,7 +14,7 @@ describe('SavePictureController', () => {
     mimeType = 'image/png'
     file = { buffer, mimeType }
     userId = 'any_user_id'
-    changeProfilePicture = jest.fn()
+    changeProfilePicture = jest.fn().mockResolvedValue({ initials: 'any_initials', pictureUrl: 'any_url' })
   })
 
   beforeEach(() => {
@@ -98,6 +98,16 @@ describe('SavePictureController', () => {
     await sut.handle({ file, userId })
 
     expect(changeProfilePicture).toHaveBeenCalledWith({ id: userId, file: buffer })
+    expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return 200 with valid data', async () => {
+    const httpResponse = await sut.handle({ file, userId })
+
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      data: { initials: 'any_initials', pictureUrl: 'any_url' }
+    })
     expect(changeProfilePicture).toHaveBeenCalledTimes(1)
   })
 })
