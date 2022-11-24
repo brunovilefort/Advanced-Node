b2553bcbebcb5f921b551240846e2c2247976613
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-24 13:22:33 -0300

    feat: ensure PUT /users/picture returns 200 on success

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,10 +1,10 @@
 import { auth } from '@/main/middlewares'
-import { adaptExpressRoute as adapt } from '@/main/adapters'
+import { adaptExpressRoute as adapt, adaptMulter as multer } from '@/main/adapters'
 import { makeSavePictureController } from '@/main/factories'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.delete('/users/picture', auth, adapt(makeSavePictureController()))
-  router.put('/users/picture', auth)
+  router.put('/users/picture', auth, multer, adapt(makeSavePictureController()))
 }
