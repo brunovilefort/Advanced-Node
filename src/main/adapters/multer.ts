34a649b62bbccb0d80e1f11b8d16876ae3f301b7
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-24 11:36:31 -0300

    feat: ensure MulterAdapter calls single upload with correct input

---
+++ src/main/adapters/multer.ts
@@ -0,0 +1,7 @@
+import { RequestHandler } from 'express'
+import multer from 'multer'
+
+export const adaptMulter: RequestHandler = (req, res, next) => {
+  const upload = multer().single('picture')
+  upload(req, res, () => {})
+}
