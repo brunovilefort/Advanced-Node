569bdabcb23c5c69fc0ab4eeb516c43c6b2268ee
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-24 14:32:55 -0300

    feat: ensure PgConnection creates a new connection

--- src/infra/postgres/helpers/connection.ts
+++ src/infra/postgres/helpers/connection.ts
@@ -1,3 +1,5 @@
+import { createConnection } from 'typeorm'
+
 export class PgConnection {
   private static instance?: PgConnection
 
@@ -6,5 +8,10 @@ export class PgConnection {
   static getInstance (): PgConnection {
     if (PgConnection.instance === undefined) PgConnection.instance = new PgConnection()
     return PgConnection.instance
+  }
+
+  async connect (): Promise<void> {
+    const connection = await createConnection()
+    connection.createQueryRunner()
   }
 }
