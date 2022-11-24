86e08dbc279983b37230db25004654068f58cd01
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-23 21:20:35 -0300

    feat: add aws s3 integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,14 @@
 export const env = {
   facebook: {
-    clientId: process.env.FB_CLIENT_ID ?? '3262311390765568',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '9a67fac4ad27e0b8f69a0148391dfad3',
-    facebookToken: process.env.FB_TOKEN ?? 'EAAuXDgZBX4gABAPYNCw20eUXWe9fhKv598MY62L5Usxxg2XT8AyOv69itYlqCfcSO8IQBfOdZAWHpSSQVdsTZB4jV2cF5g2WJgytqahkOgNKSpGBvOvPvO4SmZB7c2KWNOCxrdG0ISwdhr1NmsV5K846BGgSvrp9mmVIZBN9VILbga19aliZC6wdvEWt9tfz1nxfn9nsKlq4sUh1f9VnI8'
+    clientId: process.env.FB_CLIENT_ID!,
+    clientSecret: process.env.FB_CLIENT_SECRET!,
+    facebookToken: process.env.FB_TOKEN!
   },
-  port: process.env.PORT ?? '3333',
-  jwtSecret: process.env.JWT_SECRET ?? 'pnweargwiaru'
+  s3: {
+    accessKey: process.env.AWS_S3_ACCESS_KEY!,
+    secret: process.env.AWS_S3_SECRET!,
+    bucket: process.env.AWS_S3_BUCKET!
+  },
+  port: process.env.PORT!,
+  jwtSecret: process.env.JWT_SECRET!
 }
