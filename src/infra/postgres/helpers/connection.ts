e0d74e90cdd423d8b91498a7654cfc38142e581d
Bruno Vilefort <bruno.clara@yahoo.com>
2022-11-24 14:13:44 -0300

    feat: ensure PgConnection is a singleton

---
+++ src/infra/postgres/helpers/connection.ts
@@ -0,0 +1,10 @@
+export class PgConnection {
+  private static instance?: PgConnection
+
+  private constructor () {}
+
+  static getInstance (): PgConnection {
+    if (PgConnection.instance === undefined) PgConnection.instance = new PgConnection()
+    return PgConnection.instance
+  }
+}
